cf71af911c4000c522662eebb9442fe9b7c223a0
Xav Paice <xav.paice@canonical.com>
2021-03-26 15:39:53 +1300

    fix hacluster functest

--- tests/functional/tests/base.py
+++ tests/functional/tests/base.py
@@ -11,10 +11,11 @@ from juju_verify import juju_verify
 class BaseTestCase(unittest.TestCase):
     """Base class for functional testing of verifiers."""
 
-    def __init__(self):
-        """Set basic generic items for all tests."""
-        super().__init__()
-        self.log_level = 'info'
-        self.check = 'shutdown'
-        juju_verify.config_logger(self.log_level)
-        self.model = loop.run(juju_verify.connect_model(zaza.model.CURRENT_MODEL))
+    @classmethod
+    def setUpClass(cls):
+        """Run class setup for running tests."""
+        super(BaseTestCase, cls).setUpClass()
+        cls.log_level = 'info'
+        cls.check = 'shutdown'
+        juju_verify.config_logger(cls.log_level)
+        cls.model = loop.run(juju_verify.connect_model(zaza.model.CURRENT_MODEL))
