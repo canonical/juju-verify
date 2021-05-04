8c273f12bf9806ac711419cbdc4390c50f636cb6
Robert Gildein <robert.gildein@canonical.com>
2021-05-04 19:14:15 +0200

    logged each verifier result
     * logged each verifier result to get more information
     * fixed typo
     * removed unnecessary code

--- tests/functional/tests/test_ceph.py
+++ tests/functional/tests/test_ceph.py
@@ -1,11 +1,13 @@
 """Test deployment and functionality of juju-verify."""
+import logging
 
 from juju import loop
-import zaza.model
 from tests.base import BaseTestCase
 
 from juju_verify import juju_verify
 from juju_verify.verifiers import get_verifier
+
+logger = logging.getLogger()
 
 
 class CephTests(BaseTestCase):
@@ -24,32 +26,29 @@ class CephTests(BaseTestCase):
         unit_objects = loop.run(juju_verify.find_units(self.model, units))
         verifier = get_verifier(unit_objects)
         result = verifier.verify(check)
-        self.assertTrue(result.success is True)
+        logger.info("result: %s", result)
+        self.assertTrue(result.success)
 
     def test_single_mon_unit(self):
         """Test that shutdown of a single mon unit returns OK."""
         # juju-verify shutdown --units ceph-mon/0
 
         units = ['ceph-mon/0']
-        log_level = 'info'
         check = 'shutdown'
-        juju_verify.config_logger(log_level)
-        model = loop.run(juju_verify.connect_model(zaza.model.CURRENT_MODEL))
-        unit_objects = loop.run(juju_verify.find_units(model, units))
+        unit_objects = loop.run(juju_verify.find_units(self.model, units))
         verifier = get_verifier(unit_objects)
         result = verifier.verify(check)
-        self.assertTrue(result.success is True)
+        logger.info("result: %s", result)
+        self.assertTrue(result.success)
 
     def test_two_mon_unit(self):
         """Test that shutdown of multiple mon units fails."""
         # juju-verify shutdown --units ceph-mon/0 ceph-mon/1
 
         units = ['ceph-mon/0', 'ceph-mon/1']
-        log_level = 'info'
         check = 'shutdown'
-        juju_verify.config_logger(log_level)
-        model = loop.run(juju_verify.connect_model(zaza.model.CURRENT_MODEL))
-        unit_objects = loop.run(juju_verify.find_units(model, units))
+        unit_objects = loop.run(juju_verify.find_units(self.model, units))
         verifier = get_verifier(unit_objects)
         result = verifier.verify(check)
-        self.assertTrue(result.success is False)
+        logger.info("result: %s", result)
+        self.assertFalse(result.success)
